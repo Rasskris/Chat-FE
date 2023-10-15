c696711cf41abab9a8d2f3a46c0c7429abeb21c5
Rasskris <rassoshenko.kristina@yandex.ru>
2023-10-15 19:18:10 +0300

    feat: delete notify click

--- src/utils/sendNotification.ts
+++ src/utils/sendNotification.ts
@@ -2,12 +2,9 @@ import Message from '../types/message';
 
 const sendNotification = (message: Message) => {
   if (document.hidden) {
-    const notification = new Notification('New message from Chat', {
+    new Notification('New message from Chat', {
       body: `@${message.name}: ${message.text}`,
     })
-    notification.onclick = () => {
-      window.open(import.meta.env.VITE_BASE_URL);
-    }
   }
 };
 
