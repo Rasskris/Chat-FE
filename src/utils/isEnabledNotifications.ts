125b2f299cf3d753fb89cb18a85848af5628f997
Rasskris <rassoshenko.kristina@yandex.ru>
2023-10-15 14:44:51 +0300

    fix: add return to func isEnabledNotifications

--- src/utils/isEnabledNotifications.ts
+++ src/utils/isEnabledNotifications.ts
@@ -3,7 +3,7 @@ import Message from '../types/message';
 const isEnabledNotifications = (message: Message) => {
   if (message.name !== localStorage.getItem('userName')) {
     if (!('Notification' in window)) {
-      alert('This browser does not support system notifications!')
+      return false;
     }
     if (Notification.permission === 'granted') {
       return true;
